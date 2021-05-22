8efc5cec27dbc9893872c4144ddbe80ef5e7e10a
Frank Fourlas <frank1999ipod@gmail.com>
2021-05-22 20:14:57 +0300

    DH mm to m

--- data.py
+++ data.py
@@ -2,12 +2,12 @@ import sympy as sp
 from sympy import pi, sqrt
 
 th1, th2, th3, th4, th5, th6 = sp.symbols('th1 th2 th3 th4 th5 th6')
-d1 = 183
-d4 = 221.5
-d6 = 23.7
-L2 = 210
-L3 = 30
-L5 = 5.5
+d1 = 183e-3
+d4 = 221.5e-3
+d6 = 23.7e-3
+L2 = 210e-3
+L3 = 30e-3
+L5 = 5.5e-3
 DH_Table = [
     (0,      0,      d1,     th1),
     (-pi/2,  0,      0,      th2-(pi/2)),
